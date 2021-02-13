5727a8446b96c0d34c95325224775581625baa22
kobayashi <kobax0@gmail.com>
2021-02-13 22:34:40 +0900

    ３回目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,29 +10,26 @@ class UsersController < ApplicationController
 
   # GET /users/1
   # GET /users/1.json
-  def show;end
+  def show; end
   
-
   # GET /users/new
   def new
     @user = User.new
   end
 
   # GET /users/1/edit
-  def edit;end
+  def edit; end
 
   # POST /users
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-   if @user.save
-    login(params[:email], params[:password])
-    redirect_to login_path, notice: 'User was successfully created'
-   else
-    render :new 
-   end
-    
+    if @user.save
+      login(params[:email], params[:password])
+      redirect_to login_path, notice: 'User was successfully created'
+    else
+      render :new
+    end
   end
 
   # PATCH/PUT /users/1
@@ -60,12 +57,11 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
   def set_user
     @user = User.find(params[:id])
   end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+  
   def user_params
     params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   validates :email, uniqueness: true, presence: true
-  validates :last_name, length: {maximum: 255}
+  validates :last_name, presence: true, length:{maximum:255}
+  validates :first_name, presence: true, length:{maximum:255}
   # validates :password, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
