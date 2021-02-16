3a887f864a0b4f700fa8c265f7d95bcd00079935
kobayashi <kobax0@gmail.com>
2021-02-17 08:00:36 +0900

    ２回目

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module RunteqNormal
     config.load_defaults 5.2
     I18n.config.available_locales = :ja
     I18n.default_locale = :ja
-    # config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
