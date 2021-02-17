86eed510c3d8457ea8aa7d0b5bd07dc4930e602b
kobayashi <kobax0@gmail.com>
2021-02-17 13:47:19 +0900

    ２回目

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,7 +10,7 @@ class UserSessionsController < ApplicationController
     if @user
       redirect_back_or_to(root_path, success: 'ログインしました')
     else
-      flash.now[:danger] = 'Login failed'
+      flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
