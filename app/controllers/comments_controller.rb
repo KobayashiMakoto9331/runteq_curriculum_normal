28b398aa0e57490d0529213e95c6d30f68233125
kobayashi <kobax0@gmail.com>
2021-03-07 17:06:26 +0900

    ２回目

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,12 @@ class CommentsController < ApplicationController
       redirect_to board_path(comment.board), success: t('defaults.message.created', item: Comment.model_name.human)
     else
       redirect_to board_path(comment.board), danger: t('defaults.message.not_created', item: Comment.model_name.human)
-     end
-   end
- 
-   private
- 
-   def comment_params
-     params.require(:comment).permit(:body).merge(board_id: params[:board_id])
-   end
+    end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
+end
