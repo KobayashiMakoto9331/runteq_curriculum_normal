95799f5cedd86ede3b809fa2493cfcbf46aabc60
kobayashi <kobax0@gmail.com>
2021-03-24 10:24:27 +0900

    ２回目

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :boards do
-    resources :comments, only: %i[create edit update destroy], shallow: true
-    resource :bookmarks, only: %i[create destroy]
+    resources :comments, only: %i[create destroy], shallow: true
     collection do
       get :bookmarks
     end
   end
+  resources :bookmarks, only: %i[create destroy]
 end
