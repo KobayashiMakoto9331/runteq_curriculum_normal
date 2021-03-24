c23f703c5cf620b70fb91e2c27f7a9c457190f41
kobayashi <kobax0@gmail.com>
2021-03-24 14:04:07 +0900

    3回目

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,8 +6,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    # board = current_user.bookmarks.find_by(params[:id]).board
-    @board = Board.find(params[:board_id])
+    @board = current_user.bookmarks.find_by(params[:id]).board
+    #@board = Board.find_by(params[:board_id])
     current_user.unbookmark(@board)
     # redirect_back fallback_location: root_path, success: t('.success')
   end
