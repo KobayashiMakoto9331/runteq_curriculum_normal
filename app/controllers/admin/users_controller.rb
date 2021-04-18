bd3a843355fb225a425dd0eddc1f80a49e87b2b9
kobayashi <kobax0@gmail.com>
2021-04-18 15:30:08 +0900

    3回目

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < Admin::BaseController
   before_action :set_user, only: %i[show edit update destroy]
 
   def index
-    @q = User.ransack(params[:q]) 
+    @q = User.ransack(params[:q])
     @users = @q.result(distinct: true).order(created_at: :desc)
   end
 
@@ -12,7 +12,7 @@ class Admin::UsersController < Admin::BaseController
 
   def update
     if @user.update(user_params)
-       redirect_to admin_user_path(@user), success: 'ユーザーを更新しました'
+      redirect_to admin_user_path(@user), success: 'ユーザーを更新しました'
     else
       render :edit
     end
